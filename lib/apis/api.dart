a54ca3f7a2024638b9e07e88160f38122fda33f9
t2506nzd <t2506nzd@prestadorcbmp.com.br>
2020-10-11 21:17:01 -0300

    update

---
+++ lib/apis/api.dart
@@ -0,0 +1,13 @@
+
+import 'package:http/http.dart' as http;
+
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
+class API{
+  static Future getUsers() async{
+    var url = baseUrl + "/users";
+    return await http.get(url);
+    //enzo
+    //asdfalsdfjsalj
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/lista_usuarios.dart';
 import 'package:bytebank/screens/login.dart';
+import 'package:bytebank/screens/transferencias/formulario.dart';
 import 'package:bytebank/screens/transferencias/lista.dart';
 import 'package:flutter/material.dart';
+
+import 'apis/api.dart';
 
 void main() => runApp(ByteBankApp());
 
@@ -17,11 +23,66 @@ class ByteBankApp extends StatelessWidget {
           )
 
       ),
-      home: Login(),
+      home: ListaDeTransferencia(),
     );
   }
 
 }
+
+/*
+class ListViewJson extends StatefulWidget {
+
+  @override
+  _ListViewJsonState createState() => _ListViewJsonState();
+}
+
+class _ListViewJsonState extends State<ListViewJson> {
+
+  var users = new List<ListaUsuarios>();
+
+  _getUsers(){
+    API.getUsers().then((response) {
+      setState(() {
+        Iterable lista = json.decode(response.body);
+        users = lista.map((model) => ListaUsuarios.fromJson(model)).toList();
+      });
+    });
+  }
+
+  _ListViewJsonState(){
+    _getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List view'),
+      ),
+      body: listaUsuarios(),
+    );
+  }
+
+  listaUsuarios() {
+
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, i){
+          return ListTile(
+            title: Text(
+              users[i].name,
+              style: TextStyle(fontSize: 20.0, color: Colors.black),
+            ),
+
+          );
+        });
+  }
+
+}
+*/
+
+
+
 
 
 
